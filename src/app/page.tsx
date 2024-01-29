3be7e08e1152433920de11d378f2d8e974d468b8
Pasztor Isti <isticsek@gmail.com>
2024-01-29 18:49:01 +0200

    update imports and some style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import Card from "./components/Card";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Nimrod Keresztelo</h1>
+    <>
       <Card />
-    </div>
+    </>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center border">
+      <main className="p-4 border rounded-3xl bg-stone-200 max-w-screen-md">
+        <h1>Nimród Keresztelője</h1>
+        <p>
+          Szeretettel meghívunk a kisfiunk, Nimród keresztelőjére és az azt
+          követő ebédre. Nagyon boldogak lennénk, ha velünk ünnepelnétek ezen a
+          jeles napon.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Card;
