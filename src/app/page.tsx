8cdcbd5cc0c448ddcc9fe5431a9fc59d04303438
Pasztor Isti <isticsek@gmail.com>
2024-01-29 18:26:27 +0200

    render card boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
     <div>
       <h1>Nimrod Keresztelo</h1>
+      <Card />
     </div>
   );
 }
