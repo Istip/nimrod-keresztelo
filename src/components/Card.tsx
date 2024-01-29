af7552447551453cec2debb530aec90c35344be0
Pasztor Isti <isticsek@gmail.com>
2024-01-29 19:21:31 +0200

    update main colors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ const Card = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <main className="p-8 border rounded-3xl bg-stone-200 max-w-screen-md mx-4 flex flex-col items-center justify-center gap-4">
-        <h1 className="font-bold text-stone-600 text-2xl md:text-4xl">
+        <h1 className="font-bold text-stone-700 text-2xl md:text-4xl">
           Nimród Keresztelője
         </h1>
         <p className="text-xs md:text-lg">
@@ -12,7 +12,7 @@ const Card = () => {
           követő ebédre. Nagyon boldogak lennénk, ha velünk ünnepelnétek ezen a
           jeles napon.
         </p>
-        <p className="font-bold text-stone-600">2022.05.22 (szerda), 11:00</p>
+        <p className="font-bold text-orange-600">2022.05.22 (szerda), 11:00</p>
       </main>
     </div>
   );
