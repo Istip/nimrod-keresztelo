46a98b6e59c96fcff59499938d01e804eeba7cc0
Pasztor Isti <isticsek@gmail.com>
2024-01-29 18:49:11 +0200

    remove test border

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center border">
+    <div className="w-full h-screen flex items-center justify-center">
       <main className="p-4 border rounded-3xl bg-stone-200 max-w-screen-md">
         <h1>Nimród Keresztelője</h1>
         <p>
