ed0ff1ac8f9fd9b0f06c6fab50b83d4225875444
Pasztor Isti <isticsek@gmail.com>
2024-02-07 21:45:34 +0200

    add info for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 const Card = () => {
-  // IMPORTANT
-  // Card tilt animation example: https://codesandbox.io/p/sandbox/heuristic-pine-3gomhs?file=%2FApp.js
+  const events = [
+    {
+      id: 1,
+      name: "Templomi szertartás",
+      time: "11:00",
+      location: "Bikafalva, Református templom, Fő utca 1.",
+      icon: "💒",
+    },
+    {
+      id: 2,
+      name: "Ebéd és ünnepség",
+      time: "13:00",
+      location: "Bögöz, Erdő Panzió, Fő utca 16.",
+      icon: "🍽️",
+    },
+  ];
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <main className="p-8 border rounded-3xl bg-stone-200 max-w-screen-md mx-4 flex flex-col items-center justify-center gap-4">
+      <main className="p-8 rounded-3xl bg-stone-100 max-w-screen-sm mx-4 flex flex-col items-center justify-center gap-4">
         <h1 className="font-bold text-stone-700 text-2xl md:text-4xl">
           Nimród Keresztelője
         </h1>
@@ -15,7 +29,25 @@ const Card = () => {
           követő ebédre. Nagyon boldogak lennénk, ha velünk ünnepelnétek ezen a
           jeles napon.
         </p>
-        <p className="font-bold text-orange-600">2022.05.22 (szerda), 11:00</p>
+        <p className="font-bold text-orange-500">2022.05.22 (szerda), 11:00</p>
+
+        <ul className="w-full space-y-2">
+          {events.map((event) => (
+            <li
+              key={event.id}
+              className="flex w-full items-center justify-between gap-2"
+            >
+              <p className="text-6xl">{event.icon}</p>
+              <div className="w-full flex items-center justify-between">
+                <div>
+                  <p>{event.name}</p>
+                  <p className="text-xs text-stone-400">{event.location}</p>
+                </div>
+                <p className="font-bold">{event.time}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
