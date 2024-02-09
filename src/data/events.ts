ec9a1cc323cd9e307b0a3f7ab481036b378be9db
Pasztor Isti <isticsek@gmail.com>
2024-02-09 16:30:52 +0200

    create separate events file

---
+++ src/data/events.ts
@@ -0,0 +1,16 @@
+export const events = [
+  {
+    id: 1,
+    name: "Templomi szertartás",
+    time: "11:00",
+    location: "Bikafalva, Református templom, Fő utca 1.",
+    icon: "💒",
+  },
+  {
+    id: 2,
+    name: "Ebéd és ünnepség",
+    time: "13:00",
+    location: "Bögöz, Erdő Panzió, Fő utca 16.",
+    icon: "🍽️",
+  },
+];
