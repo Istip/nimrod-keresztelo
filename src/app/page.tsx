4a713e601c2c70f92936bb36413ce18c29157d98
Pasztor Isti <isticsek@gmail.com>
2024-02-09 15:46:34 +0200

    remove scene for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Scene from "@/components/Scene";
 import Card from "../components/Card";
 
 export default function Home() {
   return (
     <>
       <Card />
-      {/* <Scene /> */}
     </>
   );
 }
