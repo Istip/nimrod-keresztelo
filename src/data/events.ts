fc9c3d1da353caac5b668390611c97ed8a0da98b
Pasztor Isti <isticsek@gmail.com>
2024-02-28 19:45:32 +0200

    update display data

--- src/data/events.ts
+++ src/data/events.ts
@@ -3,14 +3,16 @@ export const events = [
     id: 1,
     name: "Templomi szertartás",
     time: "11:00",
-    location: "Bikafalva, Református templom, Fő utca 1.",
+    location: "Bikafalva, Református templom.",
     icon: "church",
+    url: "https://www.google.com/maps/place/Biserica+Reformat%C4%83/@46.269269,25.2589754,18.42z/data=!4m6!3m5!1s0x474b166e560816cb:0x9001084f865f618d!8m2!3d46.2695454!4d25.2594768!16s%2Fg%2F11ggt2kvbt?entry=ttu",
   },
   {
     id: 2,
     name: "Ebéd és ünnepség",
     time: "13:00",
-    location: "Bögöz, Erdő Panzió, Fő utca 16.",
+    location: "Bögöz, Erdő Panzió és Étterem.",
     icon: "meal",
+    url: "https://www.google.com/maps/place/Erdo+Panzio+Hotel/@46.2607122,25.2322779,17z/data=!3m1!4b1!4m9!3m8!1s0x474b15d5a4f0d105:0x8fd68c1fb089683d!5m2!4m1!1i2!8m2!3d46.2607085!4d25.2348528!16s%2Fg%2F1q69td76q?entry=ttu",
   },
 ];
