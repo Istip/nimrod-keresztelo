5b11e844a128e9bdafde11d9cf78d5f475820834
Pasztor Isti <isticsek@gmail.com>
2024-02-29 10:45:10 +0200

    more update

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -2,7 +2,7 @@ import { events } from "@/data/events";
 import { Icon } from "./Icon";
 
 export const Back = () => (
-  <div className="rounded-xl flip-card-back p-4 bg-stone-200 flex flex-col items-center justify-center bg-front bg-cover relative">
+  <div className="rounded-xl flip-card-back p-4 bg-stone-200 flex flex-col items-center justify-center bg-back bg-cover relative">
     <Intro />
     <ul className="w-full space-y-2 my-6">
       {events.map((event) => (
@@ -26,7 +26,7 @@ const Intro = () => (
   <p>
     Szeretettel meghívunk a kisfiunk, <span className="font-bold">Nimród</span>{" "}
     keresztelőjére és az azt követő ebédre. Nagyon boldogak lennénk, ha velünk
-    ünnepelnél a kedves családoddal, ezen a jeles napon.
+    ünnepelnél a kedves családoddal ezen a jeles napon.
   </p>
 );
 
@@ -61,26 +61,6 @@ const Event = (event: {
       <div className="text-left">
         <p className="font-bold text-lg">{event.name}</p>
         <p className="text-stone-500 text-sm">{event.location}</p>
-        <a
-          href={event.url}
-          target="_blank"
-          rel="noreferrer"
-          className="flex items-center transition-all gap-0.5 hover:gap-3 text-sky-700 font-black text-xs"
-        >
-          Elérhetőség
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12.8415 12L10.1204 8.8254C9.94067 8.61574 9.96495 8.30009 10.1746 8.12038C10.3843 7.94067 10.6999 7.96495 10.8796 8.17461L13.8796 11.6746C14.0401 11.8619 14.0401 12.1382 13.8796 12.3254L10.8796 15.8254C10.6999 16.0351 10.3843 16.0593 10.1746 15.8796C9.96495 15.6999 9.94067 15.3843 10.1204 15.1746L12.8415 12Z"
-              fill="currentColor"
-            />
-          </svg>
-        </a>
       </div>
     </div>
   </li>
